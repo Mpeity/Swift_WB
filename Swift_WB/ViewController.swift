a0e7ef89928eeebd797de687d83483ae37c59d82
Mpiety <18969043105@163.com>
2020-06-29 15:33:45 +0800

    创建项目

---
+++ Swift_WB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift_WB
+//
+//  Created by 马滕亚 on 2020/6/29.
+//  Copyright © 2020 M.Y.A. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swift_WBTests/Swift_WBTests.swift
@@ -0,0 +1,34 @@
+//
+//  Swift_WBTests.swift
+//  Swift_WBTests
+//
+//  Created by 马滕亚 on 2020/6/29.
+//  Copyright © 2020 M.Y.A. All rights reserved.
+//
+
+import XCTest
+@testable import Swift_WB
+
+class Swift_WBTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
